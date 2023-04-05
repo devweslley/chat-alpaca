1f6822251024caa4b370684c9d43c44208870aee
Weslley Sobrinho <wszeroum@gmail.com>
2023-04-05 11:25:06 -0300

    refatoração do html e css da index, responsividade e menu mobile

--- public/script.js
+++ public/script.js
@@ -65,3 +65,16 @@ input.addEventListener('keyup', async (ev) => {
   const request = await fetch('/', config);
   console.log(request.status);
 });
+
+// SCRIP MENU MOBILE //
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+// SCRIPT CHAT ALPACA 7B //
